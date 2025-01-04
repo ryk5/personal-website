c584a88e4ab9786d00ddd0bb498cf19714f6e090
ryk5 <awesomeryank@gmail.com>
2025-01-03 21:58:49 -0500

    Fixed Errors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from "./components/Contact";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
